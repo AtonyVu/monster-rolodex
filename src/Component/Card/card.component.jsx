2264e20dce91f4c1af28fbbbe0147af47eef8ab9
luancauthu2907 <luancauthu@gmail.com>
2021-11-09 10:41:46 +0700

    update

--- src/Component/Card/card.component.jsx
+++ src/Component/Card/card.component.jsx
@@ -3,6 +3,7 @@ import "./card.style.css"
 export default function Card(props) {
     return (
         <div className="card-container" >
+             <img src={`https://robohash.org/`+props.name} alt={props.name}/>
              <h1 key ={props.keyname}>{props.name}</h1>
         </div>
     )
